84904a01c167c6f59b6090c6c3c91a8900a366e4
Bylos <bylos72@gmail.com>
2015-04-25 14:14:23 +0200

    Implement roll command echo to evaluate latency

--- src/c_sandbox.c
+++ src/c_sandbox.c
@@ -20,7 +20,7 @@
 #include "ctrl_loop/PID.h"
 #include "types/types.h"
 
-#define MAIN_LOOP_TIMEOUT	10.0f
+#define MAIN_LOOP_TIMEOUT	5.0f
 
 int main(void) {
 
@@ -38,15 +38,45 @@ int main(void) {
 
 	lsm_init(i2c);
 	esc_init(i2c);
-	rflink_init();
 	timeout_init();
 
 	inertial_data_t sensors_data;
 	euler_angles_t orientation_angles;
 	rf_command_t command;
 
-	int quit = 0;
+
+
+	//Set esc to 0
+	esc_set_power(ESC_BACK_LEFT, 0.0f);
+	esc_set_power(ESC_BACK_RIGHT, 0.0f);
+	esc_set_power(ESC_FRONT_LEFT, 0.0f);
+	esc_set_power(ESC_FRONT_RIGHT, 0.0f);
+	utils_sleep(1.0);
+
+	//PID variables
+	float commonPower = 00.0f ;
+	float differentialPower = 0.0f ;
+	float maxDifferentialPower = 100.0f ;
+	float targetRollRate = 0.0f ;
+	float targetRollPos = 0.00f ;
+	int rollRatePID = PID_set(0.08f, 0.20f, 0.0f, 0.0f, 50.0f) ;
+	int rollPosPID = PID_set(3.0f, 0.00f, 0.00f, 0.0f, 5000.0f) ;
+
+	printf("Start initialization\n");
 	int ahrs_update_timer = timeout_set(AHRS_UPDATE_PERIOD);
+	while(ahrs_init(lsm_inertial_read(), AHRS_MADGWICK_2015) == 0) {
+		while(timeout_passed(ahrs_update_timer) == 0);
+		timeout_unset(ahrs_update_timer);
+		ahrs_update_timer = timeout_set(AHRS_UPDATE_PERIOD);
+	}
+	printf("Stop initialization\n");
+	//initialization of ahrs
+
+
+	rflink_init();
+
+	//timers
+	int quit = 0;
 	int rf_update_timer = timeout_set(RF_UPDATE_PERIOD);
 	int main_loop_timer = timeout_set(MAIN_LOOP_TIMEOUT);
 
@@ -55,17 +85,37 @@ int main(void) {
 		if(timeout_passed(ahrs_update_timer)) {
 			timeout_unset(AHRS_UPDATE_PERIOD);
 			ahrs_update_timer = timeout_set(AHRS_UPDATE_PERIOD);
+
+			//Calculate new orientation
 			sensors_data = lsm_inertial_read();
 			orientation_angles = ahrs_orientation_update(sensors_data, AHRS_MADGWICK_2015);
+
+			//Update PID
+			if(commonPower>=10.0f) {
+				targetRollRate = updatePID(rollPosPID, targetRollPos, orientation_angles.roll);
+				differentialPower = updatePID(rollRatePID,targetRollRate,sensors_data.gyro.x);//orientation_angles.roll) ;
+				if (differentialPower>=0) {
+					 differentialPower = min(differentialPower, maxDifferentialPower) ;
+				} else {
+					 differentialPower = max(differentialPower, -maxDifferentialPower) ;
+				}
+				esc_set_power(0,min(max(commonPower-differentialPower,0.0f),100.0f));
+				esc_set_power(1,min(max(commonPower+differentialPower,0.0f),100.0f));
+			} else {
+				esc_set_power(0,0.0f);
+				esc_set_power(1,0.0f);
+			}
 		}
 
 		if(timeout_passed(rf_update_timer)) {
 			timeout_unset(rf_update_timer);
 			rf_update_timer = timeout_set(RF_UPDATE_PERIOD);
-			rflink_orientation_send(orientation_angles);
+			//rflink_orientation_send(orientation_angles);
+			euler_angles_t temp = {0.0f, 0.0f, targetRollPos};
+			rflink_orientation_send(temp);
 		}
 
-		while((command = rflink_command_check()) != RF_CMD_NONE) {
+		if((command = rflink_command_check()) != RF_CMD_NONE) {
 			timeout_unset(main_loop_timer);
 			main_loop_timer = timeout_set(MAIN_LOOP_TIMEOUT);
 
@@ -75,12 +125,13 @@ int main(void) {
 				quit = 1;
 				break;
 			case RF_CMD_STOP:
-				esc_disable_all();
+				//esc_disable_all();
 				break;
 			case RF_CMD_ESC:
 				cmd_esc = rflink_read_esc();
 				if (cmd_esc.percent_value < 0 || cmd_esc.percent_value > 100) {
-					esc_disable(cmd_esc.esc_position);
+					//esc_disable(cmd_esc.esc_position);
+					esc_set_power(cmd_esc.esc_position, 0.0f);
 				}
 				else {
 					esc_set_power(cmd_esc.esc_position, cmd_esc.percent_value);
@@ -88,15 +139,14 @@ int main(void) {
 				break;
 
 			case RF_CMD_THROTTLE:
-				//TODO: Implement ESC common mode (throttle)
+				commonPower = rflink_read_throttle();
 				break;
 
 			case RF_CMD_PITCH:
-				//TODO: Implement Pitch control loop
 				break;
 
 			case RF_CMD_ROLL:
-				//TODO: Implement Roll control loop
+				targetRollPos = rflink_read_roll();
 				break;
 
 			default:
@@ -104,7 +154,7 @@ int main(void) {
 			}
 		}
 
-		utils_sleep(1.0);
+		//utils_sleep(1.0);
 	}
 
 	timeout_unset(ahrs_update_timer);
@@ -112,7 +162,15 @@ int main(void) {
 	timeout_unset(main_loop_timer);
 	timeout_deinit();
 
-	esc_deinit();
+	//les esc ne sont pas desactives mais simplement mis a 0 en oneshot 125
+	//esc_deinit();
+	esc_set_power(ESC_BACK_LEFT, 0.0f);
+	esc_set_power(ESC_BACK_RIGHT, 0.0f);
+	esc_set_power(ESC_FRONT_LEFT, 0.0f);
+	esc_set_power(ESC_FRONT_RIGHT, 0.0f);
+
+
+	printf("deinit uart\n");
 	uart_deinit();
 
 	mraa_result_print(mraa_i2c_stop(i2c));
